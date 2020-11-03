5137f121051220cc99ea909803cafcca00c17854
anas zahed <anas90zh@gmail.com>
2020-11-03 13:50:20 +0100

    initial commit

---
+++ gitTest/src/gitTest/Bauern.java
@@ -0,0 +1,27 @@
+package gitTest;
+
+import java.util.ArrayList;
+
+public class Bauern {
+
+	private String name ;
+	private static ArrayList<Tier> ar = new ArrayList<>();
+
+	public Bauern(String name, ArrayList<Tier> ar) {
+		super();
+		this.name = name;
+		ar.add(new Tier("cow",10));
+		ar.add(new Tier("hase",5));
+		ar.add(new Tier("schaffe",20));
+		ar.add(new Tier("Hund",10));
+
+	
+	}
+
+	public static void main(String[] args) {
+		for(Tier t:ar)
+			System.out.println("name "+ t.getName()+" Alter " + t.getAlter() );
+	}
+	
+	
+}
---
+++ gitTest/src/gitTest/Tier.java
@@ -0,0 +1,19 @@
+package gitTest;
+
+public class Tier {
+	private String name;
+	private int alter;
+	public Tier(String name, int alter) {
+		super();
+		this.name = name;
+		this.alter = alter;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAlter() {
+		return alter;
+	}
+	
+	
+}
