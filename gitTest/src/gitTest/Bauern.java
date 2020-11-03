5d510d1d3ff651ed65cc7ba04af3ea69eca58757
anas zahed <anas90zh@gmail.com>
2020-11-03 14:14:12 +0100

    changes

--- gitTest/src/gitTest/Bauern.java
+++ gitTest/src/gitTest/Bauern.java
@@ -7,9 +7,8 @@ public class Bauern {
 	private String name ;
 	private static ArrayList<Tier> ar = new ArrayList<>();
 
-	public Bauern(String name, ArrayList<Tier> ar) {
-		super();
-		this.name = name;
+
+	public static void main(String[] args) {
 		ar.add(new Tier("cow",10));
 		ar.add(new Tier("hase",5));
 		ar.add(new Tier("schaffe",20));
