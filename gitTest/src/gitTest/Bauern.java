e5419b3ee9b41c828473860f9896c5c81a9fb489
anas zahed <anas90zh@gmail.com>
2020-11-03 15:56:12 +0100

    commit and puch

--- gitTest/src/gitTest/Bauern.java
+++ gitTest/src/gitTest/Bauern.java
@@ -13,7 +13,7 @@ public class Bauern {
 		ar.add(new Tier("hase",5));
 		ar.add(new Tier("schaffe",20));
 		ar.add(new Tier("Hund",10));
-		///new master
+		////new master
 
 		for(Tier t:ar)
 			System.out.println("name "+ t.getName()+" Alter " + t.getAlter() );
