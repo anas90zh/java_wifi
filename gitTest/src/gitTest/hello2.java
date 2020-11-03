e1574cc9ffbd58451740a40e0f03fa9d086ae699
anas zahed <anas90zh@gmail.com>
2020-11-03 12:23:10 +0100

    initial commit

---
+++ gitTest/src/gitTest/hello2.java
@@ -0,0 +1,26 @@
+package gitTest;
+
+public class hello2 {
+	private String name;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public hello2(String name) {
+		super();
+		this.name = name;
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("blabla vla ");
+	}
+}
